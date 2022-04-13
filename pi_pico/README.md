7f364e44bba5468b6e97f915261b470c7cfbbacd
Yukai Li <yukaili.geek@gmail.com>
2022-04-12 22:30:02 -0600

    Add firmware for Pi Pico

--- pi_pico/README.md
+++ pi_pico/README.md
@@ -1,1 +1,93 @@
-Coming soon
+Digimon Xros Loader Code Crown Generator for Raspberry Pi Pico
+==============================================================
+
+The firmware for Raspberry Pi Pico will convert a compatible SD card into a
+Code Crown Zero.
+
+Hardware connection
+-------------------
+
+### Bill of materials
+
+- 1x Raspberry Pi Pico
+- 1x SD card/microSD card breakout board (3.3v compatible)
+- 1x Red LED
+- 1x Resistor (for current limiting the LED; please calculate the value based
+  on your LED's datasheet, but in general a 330Ω resistor will work)
+- 1x Toggle switch or button if your breakout board does not have card detect
+  broken out
+- 1x Breadboard (optional)
+- Hookup wires
+
+### Hardware connection
+
+1. Connect the `3V3` pin on the Pico to your positive power rail and any `GND`
+   pin to your ground rail.
+2. Connect the microSD card breakout to the Pico as follows:
+   - Pico pin 21 to `DAT0` or `MISO`
+   - Pico pin 22 to `DAT3` or `CS`
+   - Pico pin 24 to `CLK` or `CLK`
+   - Pico pin 25 to `CMD` or `MOSI`
+   - Pico pin 26 to card detect. Note, if your breakout board does not have a
+     card detect pin, you can connect a toggle switch or button to pin 26, and
+     connect the other side to ground.
+3. Connect the red LED such that the anode end has the Pico's pin 6 upstream.
+   Connect a resistor before or after the LED, and connect the loose end of the
+   LED or resistor to ground.
+
+Software setup
+--------------
+
+1. Install MicroPython on to your Pi Pico
+   - Download a recent build from [here](https://micropython.org/download/rp2-pico/)
+   - Hold the `BOOTSEL` button on your Pico, then plug the Pico into your
+     computer while still holding the button. You can release the button
+     once the Pico shows up on your computer as a USB drive.
+   - Drag the .uf2 firmware you have downloaded to the USB drive, and the
+     Pico will install the firmware and reboot
+2. Upload the code in the [firmware](firmware) directory to the Pico
+   - I use [Thonny](https://thonny.org/). After you install it, open it up
+     and click the button at the bottom right of the screen. Select
+     `MicroPython (Raspberry Pi Pico)`.
+   - Open up each file in the firmware directory, and select `File ->
+     Save copy...` from the menu strip. Click `Raspbery Pi Pico` when asked
+     where to save to. Type in the name of the file in the `File name` box,
+     and click `OK`.
+   - Note `sdcard.py` is in a folder. Due to how search paths works, you can
+     either put it into the root of the Pico, or create a new directory named
+     `lib` and place it in there.
+3. Replug the Pico and verify the code is installed
+   - The red LED should be lit
+
+Usage
+-----
+
+Simply insert your SD card into the slot. If you only have a microSD slot
+available but you only have a full-size SD card, insert the SD card into the
+SD to microSD adapter first, then insert the microSD end to the slot attached
+to the Pico. The green LED on the Pico will light up while it's processing,
+and extinguish after it's done. If the operation was successful, the red LED
+will stay solid, otherwise it will blink off an error code:
+
+- 1 blink: failed to read card registers
+- 2 blinks: card is formatted GPT; it must be reformatted with a MBR partition
+  table
+- 3 blinks: card does not have a partition in the first partition slot; you
+  must add a partition
+- 4 blinks: there is not enough free space after the first partition; you need
+  to shrink or repartition the first partition so there is over 1MB of
+  unpartitioned space immediately after it
+- 5 blinks: read/write error, replug the card to try again
+- 6 blinks: CSD version mismatch: the Xros Loader will probably not recognize
+  this card
+
+More detailed error messages are printed to the default UART port. You can
+also view the messages in Thonny.
+
+If your SD breakout board does not have a
+card detect pin, then you can use the switch/button alternative. Flip the
+switch or hold the button after you have inserted a card. The process will
+complete and the red LED will flash if there are errors. If you are using a
+button, make sure to hold the button until the error has finished blinking, if
+applicable. Flip the switch again or release the button before removing the
+card.
---
+++ pi_pico/main.py
@@ -0,0 +1,218 @@
+import machine
+import os
+import sdcard
+import struct
+import time
+
+# Hardware definitions ========================================================
+
+# LEDs
+led_green = machine.Pin(25, machine.Pin.OUT)
+led_red = machine.Pin(4, machine.Pin.OUT)
+
+# Card detect
+card_detect = machine.Pin(20, machine.Pin.IN, machine.Pin.PULL_UP)
+
+# SPI
+spi_sck = machine.Pin(18)
+spi_mosi = machine.Pin(19)
+spi_miso = machine.Pin(16)
+spi_cs = machine.Pin(17, machine.Pin.OUT)
+
+spi = machine.SPI(0, baudrate=2_000_000, sck=spi_sck, mosi=spi_mosi,
+                  miso=spi_miso)
+
+# Main logic ==================================================================
+
+LED_BLINK_TIME_MS = 250
+
+MAGIC = b'BGSASTNHOD01A02I'
+GPT_MAGIC = b'EFI PART'
+
+ERR_OK = 0
+ERR_REG_READ_ERROR = 1
+ERR_IS_GPT = 2
+ERR_NO_FIRST_PART = 3
+ERR_NO_SPACE = 4
+ERR_EIO = 5
+ERR_CSD_VER = 6
+
+# https://forum.micropython.org/viewtopic.php?t=1420#p8788
+@micropython.asm_thumb
+def reverse(r0, r1):               # bytearray, len(bytearray)
+    add(r4, r0, r1)
+    sub(r4, 1) # end address
+    label(LOOP)
+    ldrb(r5, [r0, 0])
+    ldrb(r6, [r4, 0])
+    strb(r6, [r0, 0])
+    strb(r5, [r4, 0])
+    add(r0, 1)
+    sub(r4, 1)
+    cmp(r4, r0)
+    bpl(LOOP)
+
+
+def generate_security_sector(cid, csd, ssr):
+    blob = bytearray(os.urandom(512))
+
+    reverse(cid, 16)
+    reverse(csd, 16)
+
+    start_offset = cid[0]
+
+    for i in range(len(MAGIC)):
+        blob[start_offset + i] = MAGIC[i] ^ cid[i]
+    
+    ssr_sum = 0
+    for i in range(2, 14):
+        ssr_sum += ssr[i]
+    ssr_sum &= 0xffff
+
+    blob[start_offset + 0x10] = (ssr_sum ^ cid[0]) & 0xff
+    blob[start_offset + 0x11] = ((ssr_sum >> 8) ^ csd[0]) & 0xff
+    blob[start_offset + 0x12] = cid[0] ^ csd[0]
+
+    return blob
+
+
+def get_crown_lba(card):
+    sector_buffer = bytearray(512)
+
+    # Note: block numbers are in bytes for SDCard driver, not sectors!
+    block_size = card.ioctl(5, None)
+
+    # 1. Check that the disk is not GPT
+    card.readblocks(1 * (512 // block_size), sector_buffer)
+    if sector_buffer == GPT_MAGIC:
+        print('Cannot process GPT disks.')
+        return -ERR_IS_GPT
+    
+    # We should also check whether this is MBR, but strictly speaking the 55aa
+    # marker is not required for non-bootable disks
+
+    # 2. Find offset of end of first partition. We'll allow multiple partitions
+    # only as long as there's enough of a gap to fit Code Crown data. If the
+    # first partition stretches all the way to the end of disk, also fail.
+    card.readblocks(0 * (512 // block_size), sector_buffer)
+    if sector_buffer[0x1be + (0 * 16) + 4] == 0:
+        print('First partition cannot be free.')
+        return -ERR_NO_FIRST_PART
+
+    partitions = []
+    for i in range(4):
+        if sector_buffer[0x1be + (i * 16) + 4] == 0:
+            continue
+
+        offset = 0x1be + (i * 16) + 8
+        start_lba = struct.unpack('<I', sector_buffer[offset:offset + 4])[0]
+        offset += 4
+        sector_length = struct.unpack('<I', sector_buffer[offset:offset + 4])[0]
+        partitions.append((start_lba, start_lba + sector_length))
+    
+    # 3. Check for free space. Requires 1MB + 512 byte = 0x801 sectors of space.
+    # Strictly speaking only 0xe0200 bytes are needed, but downloader will use
+    # full 1MB
+    if len(partitions) == 1:
+        end_lba = card.ioctl(4, None) * block_size // 512
+        if end_lba - partitions[0][1] < 0x801:
+            print('Not enough space after first partition for Code Crown data.')
+            return -ERR_NO_SPACE
+        return partitions[0][1]
+    else:
+        if partitions[1][0] - partitions[0][1] < 0x801:
+            print('Not enough space after first partition and before next partition for Code Crown data.')
+            return -ERR_NO_SPACE
+        return partitions[0][1]
+ 
+
+def write_security_sector():
+    print('Starting process')
+
+    card = sdcard.SDCard(spi, spi_cs)
+    if card.csd_version != 1:
+        print('CSD is not v1, card will not work in Xros Loader.')
+        return -ERR_CSD_VER
+
+    cid = bytearray(16)
+    csd = bytearray(16)
+    ssr = bytearray(64)
+
+    # Read CID
+    if card.cmd(10, 0, 0, 0, False) != 0:
+        spi_cs.value(1)
+        print('Error getting CID')
+        return -ERR_REG_READ_ERROR
+    card.readinto(cid)
+
+    # Read CSD
+    if card.cmd(9, 0, 0, 0, False) != 0:
+        spi_cs.value(1)
+        print('Error getting CSD')
+        return -ERR_REG_READ_ERROR
+    card.readinto(csd)
+
+    # Read SSR
+    if card.cmd(55, 0, 0) != 0 or card.cmd(13, 0, 0, 0, False) != 0:
+        spi_cs.value(1)
+        print('Error getting SSR')
+        return -ERR_REG_READ_ERROR
+    card.readinto(ssr)
+
+    lba = get_crown_lba(card)
+    if (lba < 0):
+        return lba
+    sec = generate_security_sector(cid, csd, ssr)
+    card.writeblocks(lba * (512 // card.ioctl(5, None)), sec)
+    
+    print('Process complete')
+    return ERR_OK
+
+
+def blink_err(times):
+    # Blink off
+    for _ in range(times):
+        led_red.value(0)
+        time.sleep_ms(LED_BLINK_TIME_MS)
+        led_red.value(1)
+        time.sleep_ms(LED_BLINK_TIME_MS)
+
+
+# Main loop ===================================================================
+
+# Red LED on by default to indicate power
+led_red.value(1)
+led_green.value(0)
+
+while True:
+    # Wait until card is inserted (reads low)
+    while card_detect.value() != 0:
+        time.sleep_ms(50)
+
+    # Light LED to indicate we are running
+    led_green.value(1)
+    print('Card inserted')
+    # Wait a bit in case card is still being inserted
+    time.sleep_ms(200)
+
+    # Let's go!
+    try:
+        ret = write_security_sector()
+    except Exception as ex:
+        print('Exception caught: ' + str(ex))
+        ret = -ERR_EIO
+
+    # Done, extinguish LED
+    led_green.value(0)
+
+    ret = -ret
+    if ret != ERR_OK:
+        # Blink error until card removed
+        while card_detect.value() == 0:
+            blink_err(ret)
+            time.sleep_ms(1000)
+    else:
+        # Wait until card is removed (reads high)
+        while card_detect.value() == 0:
+            time.sleep_ms(50)
+    print('Card removed')
